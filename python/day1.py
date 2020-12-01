4e67b0752509636460ae6f96ff6e5b1b5ca4c67d
Marc Abegon <marc.abegon@gmail.com>
2020-12-01 17:22:05 +0100

    Day 1_2

--- python/day1.py
+++ python/day1.py
@@ -2,7 +2,7 @@ input_path = './input_day1_1.txt'
 
 class Day1():
     @staticmethod
-    def foundEntries(input):
+    def foundTwoEntries(input):
         for entry1 in input:
             for entry2 in input:
                 if entry1 != entry2 and ((entry1 + entry2) == 2020):
@@ -10,8 +10,21 @@ class Day1():
         return [None]
 
     @staticmethod
+    def foundThreeEntries(input):
+        for entry1 in input:
+            for entry2 in input:
+                if entry1 != entry2:
+                    for entry3 in input:
+                        if entry1 != entry3 and entry2 != entry3 and ((entry1 + entry2+ entry3) == 2020):
+                            return [entry1, entry2, entry3]
+        return [None]
+    @staticmethod
     def computeResult1_1(entry_list):
         return entry_list[0] * entry_list[1]
+
+    @staticmethod
+    def computeResult1_2(entry_list):
+        return entry_list[0] * entry_list[1] * entry_list[2]
 
 def main():
     input1 = []
@@ -19,8 +32,10 @@ def main():
         for entry_input in input_fp:
             input1.append(int(entry_input))
 
-    result_1 = Day1.computeResult1_1(Day1.foundEntries(input1))
+    result_1 = Day1.computeResult1_1(Day1.foundTwoEntries(input1))
+    result_2 = Day1.computeResult1_2(Day1.foundThreeEntries(input1))
     print(f'Result 1 = {result_1}')
+    print(f'Result 2 = {result_2}')
 
 
 if __name__ == '__main__':
--- python/test_day1.py
+++ python/test_day1.py
@@ -5,23 +5,35 @@ from day1 import Day1
 class TestDay1(TestCase):
     def test_must_return_two_values(self):
         input = [1721, 979, 366, 299, 675, 1456]
-        entry_list = Day1.foundEntries(input)
+        entry_list = Day1.foundTwoEntries(input)
         self.assertEqual(2, len(entry_list))
 
     def test_found_two_entries_that_sum_2020(self):
         input = [1721, 979, 366, 299, 675, 1456]
-        entry_list = Day1.foundEntries(input)
+        entry_list = Day1.foundTwoEntries(input)
 
         self.assertEqual(2020, entry_list[0] + entry_list[1])
 
     def test_the_values_must_be_inside_input_list(self):
         input = [1721, 979, 366, 299, 675, 1456]
-        entry_list = Day1.foundEntries(input)
+        entry_list = Day1.foundTwoEntries(input)
 
         self.assertIn(entry_list[0], input)
         self.assertIn(entry_list[1], input)
 
     def test_compute_result_is_514579(self):
         input = [1721, 979, 366, 299, 675, 1456]
-        result = Day1.computeResult1_1(Day1.foundEntries(input))
+        result = Day1.computeResult1_1(Day1.foundTwoEntries(input))
         self.assertEqual(514579, result)
+
+    def test_must_return_three_values(self):
+        input = [1721, 979, 366, 299, 675, 1456]
+        entry_list = Day1.foundThreeEntries(input)
+        self.assertEqual(3, len(entry_list))
+
+    def test_compute_result_is_241861950(self):
+        input = [1721, 979, 366, 299, 675, 1456]
+        result = Day1.computeResult1_2(Day1.foundThreeEntries(input))
+        self.assertEqual(241861950, result)
+
+
