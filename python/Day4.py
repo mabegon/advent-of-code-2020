83a939bffe37813466314e21e671ba8f4b634cda
Marc Abegon <marc.abegon@gmail.com>
2020-12-04 19:31:07 +0100

    day4_2, done

--- python/Day4.py
+++ python/Day4.py
@@ -1,18 +1,97 @@
 import re
 
 
-def validatePassport(passport):
-    required_fields = ['byr',
-                       'iyr',
-                       'eyr',
-                       'hgt',
-                       'hcl',
-                       'ecl',
-                       'pid',
-                       'cid']
+def validate_byr(byr_value):
+    # byr (Birth Year) - four digits; at least 1920 and at most 2002.
+    try:
+        return int(byr_value) >= 1920 and int(byr_value) <= 2002
+    except:
+        return False
 
+
+def validate_iyr(iyr_value):
+    # iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+    try:
+        return int(iyr_value) >= 2010 and int(iyr_value) <= 2020
+    except:
+        return False
+
+
+def validate_eyr(eyr_value):
+    # eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+    try:
+        return int(eyr_value) >= 2020 and int(eyr_value) <= 2030
+    except:
+        return False
+
+
+def validate_hgt(hgt_value):
+    # hgt (Height) - a number followed by either cm or in:
+    # If cm, the number must be at least 150 and at most 193.
+    # If in, the number must be at least 59 and at most 76.
+    m = re.match('(?P<number>\d+)(?P<unit>cm|in)', hgt_value.lower())
+    if m is None:
+        return False
+    value = m.groupdict()
+    if value['unit'] == 'cm':
+        return int(value['number']) >= 150 and int(value['number']) <= 193
+    else:
+        return int(value['number']) >= 59 and int(value['number']) <= 76
+
+
+def validate_hcl(hcl_value):
+    # hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+    m = re.match('^#(\d|[a-f]){6}$', hcl_value.lower())
+    if m is None:
+        return False
+    else:
+        return True
+
+
+# ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+def validate_ecl(ecl_value):
+    accepted_values = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
+    return len(ecl_value) == 3 and ecl_value in accepted_values
+
+
+# pid (Passport ID) - a nine-digit number, including leading zeroes.
+def validate_pid(pid_value):
+    m = re.match('^(\d){9}$', pid_value)
+    if m is None:
+        return False
+    else:
+        return True
+
+
+def validate_cid(cid_value):
+    # cid (Country ID) - ignored, missing or not.
+    return True
+
+
+required_fields = {'byr': validate_byr,
+                   'iyr': validate_iyr,
+                   'eyr': validate_eyr,
+                   'hgt': validate_hgt,
+                   'hcl': validate_hcl,
+                   'ecl': validate_ecl,
+                   'pid': validate_pid,
+                   'cid': validate_cid}
+
+
+def is_correct_passport(passport):
     missing_fields = [field for field in required_fields if field not in passport.keys()]
     return len(missing_fields) == 0 or missing_fields == ['cid']
+
+
+def validate_fields(passport):
+    validated = True;
+    for field in passport.keys():
+        validated = validated and required_fields[field](passport[field])
+    return validated
+
+
+def validate_passport(passport):
+    return is_correct_passport(passport) and validate_fields(passport)
 
 
 def parse_passports(fp):
@@ -34,13 +113,18 @@ def parse_passports(fp):
 
 
 def main():
+    correct_passports = 0
     valid_passports = 0
+
     with open('../input_day4_1.txt') as input_fp:
         passports = parse_passports(input_fp)
         for passport in passports:
-            if (validatePassport(passport)):
+            if is_correct_passport(passport):
+                correct_passports += 1
+        for passport in passports:
+            if validate_passport(passport):
                 valid_passports += 1
-
+    print(f'Nb of correct passports is {correct_passports}/{len(passports)}')
     print(f'Nb of valid passports is {valid_passports}/{len(passports)}')
 
 
--- python/tests/test_day4.py
+++ python/tests/test_day4.py
@@ -2,58 +2,112 @@ from unittest import TestCase
 import python.Day4
 
 
-class TestDay3(TestCase):
+class TestDay4(TestCase):
     def test_must_validate_passport_example_1(self):
         passport = {'ecl': 'gry', 'pid': '860033327', 'eyr': '2020',
                     'hcl': '#fffffd', 'byr': '1937', 'iyr': '2017',
                     'cid': '147', 'hgt': '183cm'}
-        self.assertTrue(python.Day4.validatePassport(passport))
+        self.assertTrue(python.Day4.is_correct_passport(passport))
 
     def test_must_invalidate_passport_example_2(self):
         passport = {'iyr': '2013', 'ecl': 'amb', 'cid': '350',
                     'eyr': '2023', 'pid': '028048884', 'hcl': '#cfa07d',
                     'byr': '1929'}
-        self.assertFalse(python.Day4.validatePassport(passport))
+        self.assertFalse(python.Day4.is_correct_passport(passport))
 
     def test_must_validate_passport_example_3(self):
         passport = {'hcl': '#ae17e1', 'iyr': '2013',
                     'eyr': '2024', 'ecl': 'brn',
                     'pid': '760753108', 'byr': '1931',
                     'hgt': '179cm'}
-        self.assertTrue(python.Day4.validatePassport(passport))
+        self.assertTrue(python.Day4.is_correct_passport(passport))
 
     def test_must_invalidate_passport_example_4(self):
         passport = {'hcl': '#cfa07d', 'eyr': '2025', 'pid': '166559648',
                     'iyr': '2011', 'ecl': 'brn', 'hgt': '59in', }
-        self.assertFalse(python.Day4.validatePassport(passport))
+        self.assertFalse(python.Day4.is_correct_passport(passport))
 
     def test_must_parse_a_passport(self):
         passport_1 = {'ecl': 'gry', 'pid': '860033327', 'eyr': '2020',
-                    'hcl': '#fffffd', 'byr': '1937', 'iyr': '2017',
-                    'cid': '147', 'hgt': '183cm'}
+                      'hcl': '#fffffd', 'byr': '1937', 'iyr': '2017',
+                      'cid': '147', 'hgt': '183cm'}
 
         expected_passports = [passport_1]
 
         with open('./input_test_day4_simple.txt') as fp:
             passports = python.Day4.parse_passports(fp)
-            self.assertEqual(expected_passports,passports)
+            self.assertEqual(expected_passports, passports)
 
     def test_must_parse_input_passports(self):
         passport_1 = {'ecl': 'gry', 'pid': '860033327', 'eyr': '2020',
-                    'hcl': '#fffffd', 'byr': '1937', 'iyr': '2017',
-                    'cid': '147', 'hgt': '183cm'}
+                      'hcl': '#fffffd', 'byr': '1937', 'iyr': '2017',
+                      'cid': '147', 'hgt': '183cm'}
         passport_2 = {'iyr': '2013', 'ecl': 'amb', 'cid': '350',
-                    'eyr': '2023', 'pid': '028048884', 'hcl': '#cfa07d',
-                    'byr': '1929'}
+                      'eyr': '2023', 'pid': '028048884', 'hcl': '#cfa07d',
+                      'byr': '1929'}
         passport_3 = {'hcl': '#ae17e1', 'iyr': '2013',
-                    'eyr': '2024', 'ecl': 'brn',
-                    'pid': '760753108', 'byr': '1931',
-                    'hgt': '179cm'}
+                      'eyr': '2024', 'ecl': 'brn',
+                      'pid': '760753108', 'byr': '1931',
+                      'hgt': '179cm'}
         passport_4 = {'hcl': '#cfa07d', 'eyr': '2025', 'pid': '166559648',
-                    'iyr': '2011', 'ecl': 'brn', 'hgt': '59in', }
+                      'iyr': '2011', 'ecl': 'brn', 'hgt': '59in', }
 
-        expected_passports = [passport_1,passport_2, passport_3, passport_4]
+        expected_passports = [passport_1, passport_2, passport_3, passport_4]
 
         with open('./input_test_day4.txt') as fp:
             passports = python.Day4.parse_passports(fp)
-            self.assertEqual(expected_passports,passports)
+            self.assertEqual(expected_passports, passports)
+
+    def test_1920_must_be_a_valid_bry(self):
+        # byr (Birth Year) - four digits; at least 1920 and at most 2002.
+        self.assertTrue(python.Day4.validate_byr('1920'))
+
+    def test_2002_must_be_a_valid_bry(self):
+        self.assertTrue(python.Day4.validate_byr('2002'))
+
+    def test_1919_must_be_a_invalid_bry(self):
+        self.assertFalse(python.Day4.validate_byr('1919'))
+
+    def test_abcd_must_be_a_invalid_bry(self):
+        self.assertFalse(python.Day4.validate_byr('abcd'))
+
+    def test_150cm_must_be_a_valid_hgt(self):
+        self.assertTrue(python.Day4.validate_hgt('150cm'))
+
+    def test_150CM_must_be_a_valid_hgt(self):
+        self.assertTrue(python.Day4.validate_hgt('150CM'))
+
+    def test_59in_must_be_a_valid_hgt(self):
+        self.assertTrue(python.Day4.validate_hgt('59in'))
+
+    def test_150in_must_be_a_invalid_hgt(self):
+        self.assertFalse(python.Day4.validate_hgt('150in'))
+
+    def test_150f4in_must_be_a_invalid_hgt(self):
+        self.assertFalse(python.Day4.validate_hgt('150f4in'))
+
+    def test_sharp_ae17e1_must_be_a_valid_hcl(self):
+        self.assertTrue(python.Day4.validate_hcl('#ae17e1'))
+
+    def test_blu_must_be_a_valid_ecl(self):
+        self.assertTrue(python.Day4.validate_ecl('blu'))
+
+    def test_blue_must_be_a_invalid_ecl(self):
+        self.assertFalse(python.Day4.validate_ecl('blue'))
+
+    def test_000000000_must_be_a_valid_pid(self):
+        self.assertTrue(python.Day4.validate_pid('000000000'))
+
+    def test_23_must_be_a_invalid_pid(self):
+        self.assertFalse(python.Day4.validate_pid('23'))
+
+    def test_must_be_a_valid_passport(self):
+        passport = {'iyr': '2010','hgt': '158cm','hcl': '#b6652a',
+                    'ecl': 'blu', 'byr': '1944', 'eyr': '2021', 'pid': '093154719'}
+        self.assertTrue(python.Day4.validate_passport(passport))
+
+    def test_must_be_a_invalid_passport(self):
+
+        passport = {'iyr': '2012','hgt': '182cm','hcl': 'dab227', 'cid':'277',
+                    'ecl': 'brn', 'byr': '1992', 'eyr': '2020', 'pid': '021572410'}
+        self.assertFalse(python.Day4.validate_passport(passport))
