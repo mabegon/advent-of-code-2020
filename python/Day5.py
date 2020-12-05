8e2101132d24837826985b85781176e9c7cda204
Marc Abegon <marc.abegon@gmail.com>
2020-12-05 22:26:00 +0100

    day 5_2: without refactoring

--- python/Day5.py
+++ python/Day5.py
@@ -40,14 +40,24 @@ def select_seat(actions):
     return row * 8 + column
 
 
+def find_my_seat(seats):
+    for seat in sorted(seats):
+        if seat + 1 not in seats and seat + 2 in seats:
+            return seat + 1
+
+
 def main():
     highest_seat = 0
+    seats = []
     with open('../input_day5_1.txt') as input_fp:
         for actions in input_fp:
             seat = select_seat(actions.strip())
+            seats.append(seat)
             highest_seat = seat if seat > highest_seat else highest_seat
-
+    my_seat = find_my_seat(seats)
     print(f'The highest seat ID is {highest_seat}')
+    print(f'My seat is  is {my_seat}')
+
 
 if __name__ == '__main__':
     main()
