5859e99d0a760b14e663fc64c5d4e32f5403ac60
Marc Abegon <marc.abegon@gmail.com>
2020-12-14 01:26:51 +0100

    day11_2 with Python

--- python/Day11.py
+++ python/Day11.py
@@ -29,19 +29,33 @@ def must_free_seat(board, position):
     return board[y][x] == '#' and count_adjacent_occupied(board, position) >= 4
 
 
-def compute_movement(board):
+def must_occupy_seat_v2(board, position):
+    x = position[0]
+    y = position[1]
+
+    return board[y][x] == 'L' and count_first_seat_adjacent(board, position) == 0
+
+
+def must_free_seat_v2(board, position):
+    x = position[0]
+    y = position[1]
+
+    return board[y][x] == '#' and count_first_seat_adjacent(board, position) >= 5
+
+
+def compute_movement(board, occupy_seat_policy, free_seat_policy):
     new_board = []
     for y in range(0, len(board)):
         new_line = []
         for x in range(0, len(board[0])):
             value = board[y][x]
             if value == 'L':
-                if must_occupy_seat(board, (x, y)):
+                if occupy_seat_policy(board, (x, y)):
                     new_line.append('#')
                 else:
                     new_line.append('L')
             elif value == '#':
-                if must_free_seat(board, (x, y)):
+                if free_seat_policy(board, (x, y)):
                     new_line.append('L')
                 else:
                     new_line.append('#')
@@ -59,17 +73,17 @@ def parse_board(fp):
     return board
 
 
-def find_stable_state(board):
+def find_stable_state(board, occupy_seat_policy, free_seat_policy):
     current_board = None
     new_board = board
     while current_board != new_board:
         current_board = new_board
-        new_board = compute_movement(current_board)
+        new_board = compute_movement(current_board, occupy_seat_policy, free_seat_policy)
     return new_board
 
 
 def compute_result1(board):
-    stable_board = find_stable_state(board)
+    stable_board = find_stable_state(board, must_occupy_seat, must_free_seat)
     result = count_occupied_on_board(stable_board)
     return result
 
@@ -81,11 +95,55 @@ def count_occupied_on_board(stable_board):
     return result
 
 
+def out_board(board, current_column, current_row):
+    return current_column < 0 or current_column >= len(board[0]) \
+           or current_row < 0 or current_row >= len(board)
+
+
+def first_seat_occupied(board, position, column_direction, row_direction):
+    current_column = position[0]
+    current_row = position[1]
+    while not out_board(board, current_column, current_row):
+        if current_row != position[1] or current_column != position[0]:
+            if board[current_row][current_column] == '#':
+                return 1
+            elif board[current_row][current_column] == 'L':
+                return 0
+        current_column = current_column + column_direction
+        current_row = current_row + row_direction
+    return 0
+
+
+def count_first_seat_adjacent(board, position):
+    count = 0
+    column = position[0]
+    row = position[1]
+
+    count += 1 if first_seat_occupied(board, position, 0, 1) else 0
+    count += 1 if first_seat_occupied(board, position, 0, -1) else 0
+    count += 1 if first_seat_occupied(board, position, 1, 0) else 0
+    count += 1 if first_seat_occupied(board, position, -1, 0) else 0
+    count += 1 if first_seat_occupied(board, position, 1, 1) else 0
+    count += 1 if first_seat_occupied(board, position, 1, -1) else 0
+    count += 1 if first_seat_occupied(board, position, -1, 1) else 0
+    count += 1 if first_seat_occupied(board, position, -1, -1) else 0
+
+    return count
+
+
+def compute_result2(board):
+    stable_board = find_stable_state(board, must_occupy_seat_v2, must_free_seat_v2)
+    result = count_occupied_on_board(stable_board)
+    return result
+
+
 def main():
     with open('../input_day11_1.txt') as fp:
         board = parse_board(fp)
         result1 = compute_result1(board)
         print(f'Result 1 is {result1}')
+        result2 = compute_result2(board)
+        print(f'Result 2 is {result2}')
 
 
 if __name__ == '__main__':
--- python/tests/test_day11.py
+++ python/tests/test_day11.py
@@ -60,9 +60,9 @@ class TestDay11(TestCase):
         self.assertEqual(5, python.Day11.count_adjacent_occupied(board, position))
 
     def test_count_adjacents_must_be_5_for_position_4_2(self):
-        board = [['L', '#', '#','L', '#', '#'],
-                 ['#', 'L', '#','L', '#', 'L'],
-                 ['#', 'L', 'L','#', 'L', '#'],
+        board = [['L', '#', '#', 'L', '#', '#'],
+                 ['#', 'L', '#', 'L', '#', 'L'],
+                 ['#', 'L', 'L', '#', 'L', '#'],
                  ['#', 'L', 'L', '#', 'L', '#']]
         position = (4, 2)
 
@@ -107,7 +107,8 @@ class TestDay11(TestCase):
         board_expected = [['#', '#', '#'],
                           ['#', '#', '#'],
                           ['#', '#', '#']]
-        self.assertEqual(board_expected, python.Day11.compute_movement(board))
+        self.assertEqual(board_expected, python.Day11.compute_movement(board, python.Day11.must_occupy_seat,
+                                                                       python.Day11.must_free_seat))
 
     def test_must_compute_movement_2(self):
         board = [['#', 'L', 'L'],
@@ -116,16 +117,29 @@ class TestDay11(TestCase):
         board_expected = [['#', 'L', '#'],
                           ['L', 'L', '#'],
                           ['#', '#', '#']]
-        self.assertEqual(board_expected, python.Day11.compute_movement(board))
+        self.assertEqual(board_expected, python.Day11.compute_movement(board, python.Day11.must_occupy_seat,
+                                                                       python.Day11.must_free_seat))
 
     def test_must_found_stable_board(self):
         with open('input_test_day11.txt') as fp:
             board = python.Day11.parse_board(fp)
         with open('input_test_day11_stable.txt') as fp:
             expected_board = python.Day11.parse_board(fp)
-        self.assertEqual(expected_board, python.Day11.find_stable_state(board))
+        self.assertEqual(expected_board, python.Day11.find_stable_state(board, python.Day11.must_occupy_seat,
+                                                                        python.Day11.must_free_seat))
 
     def test_compute_result_must_return_37(self):
         with open('input_test_day11.txt') as fp:
             board = python.Day11.parse_board(fp)
         self.assertEqual(37, python.Day11.compute_result1(board))
+
+    def test_count_first_seat_adjacent_must_return_8(self):
+        with open('input_test_day11_2.txt') as fp:
+            board = python.Day11.parse_board(fp)
+            position = (3, 4)
+            self.assertEqual(8, python.Day11.count_first_seat_adjacent(board, position))
+
+    def test_compute_result2_must_return_26(self):
+        with open('input_test_day11.txt') as fp:
+            board = python.Day11.parse_board(fp)
+        self.assertEqual(26, python.Day11.compute_result2(board))
