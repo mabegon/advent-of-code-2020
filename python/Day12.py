7319ba11346f057860263767c15885100a89b7ba
Marc Abegon <marc.abegon@gmail.com>
2020-12-14 22:40:36 +0100

    day 12_2 in python. I must refactor code in order to apply DRY principle. An idea would be use strategy pattern

--- python/Day12.py
+++ python/Day12.py
@@ -1,3 +1,5 @@
+WAYPOINT_EAST_WEST = 'waypoint_east_west'
+WAYPOINT_NORTH_SOUTH = 'waypoint_north_south'
 WEST = 270
 SOUTH = 180
 EAST = 90
@@ -90,25 +92,34 @@ def south_action(state, value):
 
 def rotate_left_action(state, degrees):
     new_state = state.copy()
-    if new_state[ORIENTED] - abs(degrees) % 360 >= 0:
-        new_state[ORIENTED] -= abs(degrees) % 360
-    else:
-        new_state[ORIENTED] = 360 - abs(new_state[ORIENTED] - degrees) % 360
-    if new_state[ORIENTED] == 360:
-        new_state[ORIENTED] = 0
+    new_state[ORIENTED] = rotate_left(degrees, new_state[ORIENTED])
     return new_state
+
+
+def rotate_left(degrees, origin):
+    if origin - abs(degrees) % 360 >= 0:
+        result = origin - abs(degrees) % 360
+    else:
+        result = 360 - abs(origin - degrees) % 360
+    if result == 360:
+        result = 0
+    return result
 
 
 def rotate_right_action(state, degrees):
     new_state = state.copy()
-    if new_state[ORIENTED] + abs(degrees) % 360 > 360:
-        new_state[ORIENTED] = 0 + abs(new_state[ORIENTED] + degrees) % 360
-    else:
-        new_state[ORIENTED] += abs(degrees) % 360
-
-    if new_state[ORIENTED] == 360:
-        new_state[ORIENTED] = 0
+    new_state[ORIENTED] = rotate_right(degrees, new_state[ORIENTED])
     return new_state
+
+
+def rotate_right(degrees, origin):
+    if origin + abs(degrees) % 360 > 360:
+        result = 0 + abs(origin + degrees) % 360
+    else:
+        result = origin + abs(degrees) % 360
+    if result == 360:
+        result = 0
+    return result
 
 
 def compute_result1(state, commands):
@@ -117,6 +128,100 @@ def compute_result1(state, commands):
         current_state = compute_state(current_state, command)
     return abs(current_state[NORTH_SOUTH]) + abs(current_state[EAST_WEST])
 
+
+def compute_result2(state, commands):
+    current_state = state
+    for command in commands:
+        current_state = compute_state_v2(current_state, command)
+    return abs(current_state[NORTH_SOUTH]) + abs(current_state[EAST_WEST])
+
+
+def forward_action_v2(state: dict, value: int) -> dict:
+    new_state = state.copy()
+    new_state[NORTH_SOUTH] += state[WAYPOINT_NORTH_SOUTH] * value
+    new_state[EAST_WEST] += state[WAYPOINT_EAST_WEST] * value
+    return new_state
+
+
+def west_action_v2(state, value):
+    new_state = state.copy()
+    new_state[WAYPOINT_EAST_WEST] -= value
+    return new_state
+
+
+def east_action_v2(state, value):
+    new_state = state.copy()
+    new_state[WAYPOINT_EAST_WEST] += value
+    return new_state
+
+
+def north_action_v2(state, value):
+    new_state = state.copy()
+    new_state[WAYPOINT_NORTH_SOUTH] += value
+    return new_state
+
+
+def south_action_v2(state, value):
+    new_state = state.copy()
+    new_state[WAYPOINT_NORTH_SOUTH] -= value
+    return new_state
+
+
+def rotate_action(state, degrees, rotate_strategy):
+    new_state = state.copy()
+
+    degrees_north_south = 0 if new_state[WAYPOINT_NORTH_SOUTH] > 0 else 180
+    degrees_east_west = 90 if new_state[WAYPOINT_EAST_WEST] > 0 else 270
+
+    new_degrees_north_south = rotate_strategy(degrees, degrees_north_south)
+    new_degrees_east_west = rotate_strategy(degrees, degrees_east_west)
+
+    if new_degrees_north_south == 0:
+        new_state[WAYPOINT_NORTH_SOUTH] = abs(state[WAYPOINT_NORTH_SOUTH])
+    elif new_degrees_north_south == 90:
+        new_state[WAYPOINT_EAST_WEST] = abs(state[WAYPOINT_NORTH_SOUTH])
+    elif new_degrees_north_south == 180:
+        new_state[WAYPOINT_NORTH_SOUTH] = abs(state[WAYPOINT_NORTH_SOUTH]) * -1
+    elif new_degrees_north_south == 270:
+        new_state[WAYPOINT_EAST_WEST] = abs(state[WAYPOINT_NORTH_SOUTH]) * -1
+
+    if new_degrees_east_west == 0:
+        new_state[WAYPOINT_NORTH_SOUTH] = abs(state[WAYPOINT_EAST_WEST])
+    elif new_degrees_east_west == 90:
+        new_state[WAYPOINT_EAST_WEST] = abs(state[WAYPOINT_EAST_WEST])
+    elif new_degrees_east_west == 180:
+        new_state[WAYPOINT_NORTH_SOUTH] = abs(state[WAYPOINT_EAST_WEST]) * -1
+    elif new_degrees_east_west == 270:
+        new_state[WAYPOINT_EAST_WEST] = abs(state[WAYPOINT_EAST_WEST]) * -1
+
+    return new_state
+
+
+def compute_state_v2(state: dict, command: str) -> dict:
+    action, value = decode_command(command)
+    if action == 'F':
+        return forward_action_v2(state, value)
+    elif action == 'W':
+        return west_action_v2(state, value)
+    elif action == 'E':
+        return east_action_v2(state, value)
+    elif action == 'N':
+        return north_action_v2(state, value)
+    elif action == 'S':
+        return south_action_v2(state, value)
+    elif action == 'L':
+        return rotate_left_action_v2(state, value)
+    elif action == 'R':
+        return rotate_right_action_v2(state, value)
+    return None
+
+
+def rotate_right_action_v2(state, value):
+    return rotate_action(state, value, rotate_right)
+
+
+def rotate_left_action_v2(state, value):
+    return rotate_action(state, value, rotate_left)
 
 def main():
     commands = []
@@ -127,6 +232,10 @@ def main():
             commands.append(line.strip())
     result1 = compute_result1(state, commands)
     print(f'Result 1 is {result1}')
+
+    state_v2 = {WAYPOINT_NORTH_SOUTH: 1, WAYPOINT_EAST_WEST: 10, NORTH_SOUTH: 0, EAST_WEST: 0}
+    result2 = compute_result2(state_v2, commands)
+    print(f'Result 2 is {result2}')
 
 
 if __name__ == '__main__':
--- python/tests/test_day12.py
+++ python/tests/test_day12.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 import python.Day12
-from python.Day12 import NORTH_SOUTH, EAST_WEST, ORIENTED, NORTH, SOUTH, EAST, WEST
+from python.Day12 import NORTH_SOUTH, EAST_WEST, ORIENTED, NORTH, SOUTH, EAST, WEST, WAYPOINT_NORTH_SOUTH, WAYPOINT_EAST_WEST
 
 
 class TestDay12(TestCase):
@@ -50,7 +50,22 @@ class TestDay12(TestCase):
 
 
     def test_compute_result1_result_must_be_25(self):
-        #'F10', 'N3', 'F7', 'R90', 'F11'
         commands_input = ['F10', 'N3', 'F7', 'R90', 'F11']
         state = {ORIENTED: EAST, NORTH_SOUTH: 0, EAST_WEST: 0}
         self.assertEqual(25, python.Day12.compute_result1(state, commands_input))
+
+    def test_forward_10_v2_action(self):
+        state = {WAYPOINT_NORTH_SOUTH:1, WAYPOINT_EAST_WEST: 10, NORTH_SOUTH: 0, EAST_WEST: 0}
+        expected_state = {WAYPOINT_NORTH_SOUTH:1, WAYPOINT_EAST_WEST: 10, NORTH_SOUTH: 10, EAST_WEST: 100}
+        self.assertEqual(expected_state, python.Day12.forward_action_v2(state, 10))
+
+    def test_rotate_right_90_v2_action(self):
+
+        state = {WAYPOINT_NORTH_SOUTH:4, WAYPOINT_EAST_WEST: 10, NORTH_SOUTH: 38, EAST_WEST: 170}
+        expected_state = {WAYPOINT_NORTH_SOUTH:-10, WAYPOINT_EAST_WEST: 4, NORTH_SOUTH: 38, EAST_WEST: 170}
+        self.assertEqual(expected_state, python.Day12.rotate_right_action_v2(state, 90))
+
+    def test_compute_result2_result_must_be_286(self):
+        commands_input = ['F10', 'N3', 'F7', 'R90', 'F11']
+        state = {WAYPOINT_NORTH_SOUTH:1, WAYPOINT_EAST_WEST: 10, NORTH_SOUTH: 0, EAST_WEST: 0}
+        self.assertEqual(286, python.Day12.compute_result2(state, commands_input))
