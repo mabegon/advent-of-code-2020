5029f0609287c7634a452f6bb4fa7f1a6ed235ff
Marc Abegon <marc.abegon@gmail.com>
2020-12-15 17:13:10 +0100

    Day13_2 in Python.

--- python/Day13.py
+++ python/Day13.py
@@ -1,4 +1,5 @@
 import itertools
+from typing import Tuple, List
 
 
 def find_first_bus(bus_list, earliest_bus):
@@ -6,6 +7,20 @@ def find_first_bus(bus_list, earliest_bus):
         for bus in bus_list:
             if time % bus == 0:
                 return bus, time - earliest_bus
+
+
+def transform_input(bus_str: str):
+    return [(int(b), i) for i, b in enumerate(bus_str.split(',')) if b != 'x']
+
+
+def find_earliest_timestamp(bus_offsets: List[Tuple[int, int]]) -> int:
+    timestamp_step = bus_offsets[0][0]
+    t = timestamp_step
+    for bus, time_offset in bus_offsets[1:]:
+        while (t + time_offset) % bus != 0:
+            t += timestamp_step
+        timestamp_step *= bus
+    return t
 
 
 def main():
@@ -16,10 +31,12 @@ def main():
         for element in bus_str.split(','):
             if element != 'x':
                 bus_list.append(int(element))
-        print(bus_list)
         bus, minutes = find_first_bus(bus_list, earliest_bus)
-
         print(f'Result 1 for bus n. {bus} and waiting {minutes} minutes,  is {bus * minutes}')
+
+        bus_offsets = transform_input(bus_str)
+        earliest_timestamp = find_earliest_timestamp(bus_offsets)
+        print(f'The earliest timestamp is {earliest_timestamp}')
 
 
 if __name__ == '__main__':
--- python/tests/test_day13.py
+++ python/tests/test_day13.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 import python.Day13
 
+
 class TestDay13(TestCase):
     def test_find_first_bus_from_timestamp_must_be_59_and_wait_5_minutes(self):
         bus = [7, 13, 59, 31, 19]
@@ -8,3 +9,12 @@ class TestDay13(TestCase):
         bus, minutes = python.Day13.find_first_bus(bus, earliest_bus)
         self.assertEqual(59, bus)
         self.assertEqual(5, minutes)
+
+    def test_transform_bus_list_structure(self):
+        bus_str = '7,13,x,x,59,x,31,19'
+        bus_dict_expected = [(7,0), (13,1), (59,4), (31,6), (19, 7)]
+        self.assertEqual(bus_dict_expected, python.Day13.transform_input(bus_str))
+
+    def test_find_earliest_timestamp_must_be_1068781(self):
+        bus_offsets = [(7,0), (13,1), (59,4), (31,6), (19, 7)]
+        self.assertEqual(1068781, python.Day13.find_earliest_timestamp(bus_offsets))
